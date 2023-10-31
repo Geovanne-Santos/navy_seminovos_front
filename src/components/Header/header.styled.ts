47efee18ecf86fa4cab3c948975ca2fdcf2641ec
Geovanne <geovamelo4431@gmail.com>
2023-10-31 01:39:19 -0300

    feat: Toggle Icon Menu

--- src/components/Header/header.styled.ts
+++ src/components/Header/header.styled.ts
@@ -23,6 +23,7 @@ export const navbar = styled("nav")({
     cursor: "pointer"
   },
 });
+
 export const logo = styled('div')({
   width: "15rem",
   height: "auto",
@@ -35,8 +36,11 @@ export const logo = styled('div')({
   }
 });
 
+interface IconeHamburguerProps {
+  toggleMenu: boolean;
+}
 
-export const IconeHamburguer = styled('div')(( toggleMenu: boolean ) => ({
+export const IconeHamburguer = styled('div')<IconeHamburguerProps>(({ toggleMenu }) => ({
   cursor: "pointer",
   position: "relative",
   height: "30px",
@@ -45,28 +49,29 @@ export const IconeHamburguer = styled('div')(( toggleMenu: boolean ) => ({
   flexDirection: "column",
   justifyContent: "space-between",
   transition: ".3s ease-in-out",
+
   "& > div": {
+    position: 'relative',
     width: "100%",
     height: "4px",
     borderRadius: "2px",
     backgroundColor: "#545454",
     transition: ".3s ease-in-out",
+
     "&.metade": {
-      width: "70%"
+      width: toggleMenu ? "100%" : "70%",
+      opacity: toggleMenu ? "0" : "1"
     }
   },
+
   "& > div:nth-of-type(1)": {
-    transform: toggleMenu ? 'rotate(45deg)' : 'rotate(0deg)',
-    top: toggleMenu ? '10px' : '0',
+    transformOrigin: 'center',
+    transform: toggleMenu ? 'rotate(45deg) translate(7px,7px)' : '',
   },
-  "& > div:nth-of-type(2)": {
-    opacity: toggleMenu ? '0' : '1',
-  },
+
   "& > div:nth-of-type(3)": {
-    transform: toggleMenu ? 'rotate(-45deg)' : 'rotate(0deg)',
-    top: toggleMenu ? '-10px' : '0',
+    transformOrigin: 'center',
+    transform: toggleMenu ? 'rotate(-45deg) translate(7px,-7px)' : '',
   },
-  "& > div:nth-of-type(4)": {
-    transform: toggleMenu ? 'rotate(0deg)' : 'rotate(0deg)',
-  }
-}))
+}));
+
