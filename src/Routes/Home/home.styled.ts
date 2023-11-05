6cb14cdd5039b6ca3c0e36a0cc605243b29e61ac
Geovanne dos Santos <64851512+Geovanne-Santos@users.noreply.github.com>
2023-11-05 19:51:08 +0000

    Update home.styled.ts and index.tsx

--- src/Routes/Home/home.styled.ts
+++ src/Routes/Home/home.styled.ts
@@ -1,66 +1,66 @@
 import { createTheme, styled } from "@mui/material";
-import Fundo from '../../assets/fundoHome.png';
+import Fundo from "../../assets/fundoHome.png";
 
 const theme = createTheme({
-    breakpoints: {
-        values: {
-            xs: 0,
-            sm: 600,
-            md: 900,
-            lg: 1200,
-            xl: 1536,
-        },
-    }
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
 });
 
 export const secaoInicio = styled("section")({
-    width: "100%",
-    height: theme.spacing(90),
-    display: "grid",
-    placeItems: "center",
-    padding: "5rem 0"
-})
+  width: "100%",
+  height: theme.spacing(90),
+  display: "grid",
+  placeItems: "center",
+  padding: "5rem 0",
+});
 
 export const containerInicio = styled("div")({
-    width: "90%",
-    position: "relative",
-    height: "100%",
-    backgroundColor: "#FB5B4D",
-    borderRadius: "5rem",
-    borderTopLeftRadius: "20rem",
-    borderBottomRightRadius: "20rem",
-    ".card": {
-        width: "40%",
-        height: theme.spacing(20),
-        display: "grid",
-        placeItems: "center",
-        position: "absolute",
-        top: "50%",
-        left: "15%",
-        transform: "translate(-15%, -50%)",
-        backgroundColor: "#FAFAFA",
-        borderRadius: theme.spacing(1.2),
-    }
-})
-
-export const fundoCard = styled("div")({
-    width: "100%",
-    height: "100%",
-    borderRadius: theme.spacing(20),
-    backgroundColor: "#01017D",
-    position: "absolute",
-    top: "50%",
-    right: "-50%",
-    transform: "translate(0%, -50%)",
+  width: "90%",
+  height: "100%",
+  position: "relative",
+  backgroundColor: "#FB5B4D",
+  borderRadius: "5rem",
+  borderTopLeftRadius: "20rem",
+  borderBottomRightRadius: "20rem",
+  ".card": {
+    width: "20%",
+    height: theme.spacing(30),
     display: "grid",
     placeItems: "center",
-    "> div": {
-        width: "90%",
-        height: "90%",
-        backgroundImage: `url(${Fundo})`,
-        backgroundAttachment: "fixed", 
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-    }
-})
+    position: "absolute",
+    top: "50%",
+    left: "15%",
+    transform: "translate(-15%, -50%)",
+    backgroundColor: "#FAFAFA",
+    borderRadius: theme.spacing(1.2),
+  },
+});
+
+export const fundoCard = styled("div")({
+  width: "100%",
+  height: "100%",
+  borderRadius: theme.spacing(20),
+  backgroundColor: "#01017D",
+  position: "absolute",
+  top: "50%",
+  right: "-50%",
+  transform: "translate(0%, -50%)",
+  display: "grid",
+  placeItems: "center",
+  "> div": {
+    width: "90%",
+    height: "90%",
+    backgroundImage: `url(${Fundo})`,
+    backgroundAttachment: "fixed",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+  },
+});
--- src/Routes/Home/index.tsx
+++ src/Routes/Home/index.tsx
@@ -1,22 +1,38 @@
-import Skeleton from '@mui/material/Skeleton';
-import * as Style from './home.styled';
+import { useEffect } from "react";
+import { Autocomplete } from "../../../node_modules/@mui/material/index";
+import * as Style from "./home.styled";
 
 export const Home = () => {
+
+  useEffect({
+    let url = 'https://servicodados.ibge.gov.br/api/v1/localidades/distritos';
+  }, [])
+
+
   return (
     <Style.secaoInicio>
       <Style.containerInicio>
-        <div className='card'>
-          <Skeleton variant="rounded" animation="wave" />
+        <div className="card">
+          <h2>
+            CONFIE NA <span>NAVY</span> PARA ENCONTRAR SEMINOVOS DE VERDADE!
+          </h2>
+          <div className="secao-card">
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={top100Films}
+              sx={{ width: 300 }}
+              renderInput={(params) => (
+                <TextField {...params} label="Selecione a cidade" />
+              )}
+            />
+          </div>
         </div>
-        {/*
-        
+
         <Style.fundoCard>
           <div></div>
-        </Style.fundoCard> 
-        
-        */}
-
+        </Style.fundoCard>
       </Style.containerInicio>
     </Style.secaoInicio>
-  )
-}
+  );
+};
